f9e5e2405a460c2aac9e917ff776118171f5ea0c
StrikerOne65 <golamrabbani20014@gmail.com>
2022-04-07 17:33:22 +0530

    First auth started

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <Link href='/login'>
+        <a>Login</a>
+      </Link>
+
       <footer className={styles.footer}>
         <a
           href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
--- pages/login.js
+++ pages/login.js
@@ -1,3 +1,25 @@
+import { signInWithPopup } from 'firebase/auth'
+import { auth, googleProvider } from '../lib/firebase'
+import Link from 'next/link'
+
 export default function Login() {
-  return <h1>Login Page</h1>
+  const handleSignin = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        console.log(result.user)
+      })
+      .catch((err) => {
+        console.log('Something went wrong', err)
+      })
+  }
+
+  return (
+    <>
+      <h1>Login Page</h1>
+      <button onClick={handleSignin}>Sign in with Google</button>
+      <Link href='/'>
+        <a>Home</a>
+      </Link>
+    </>
+  )
 }
