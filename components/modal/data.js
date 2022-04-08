839de87bf2e990eac119fe6be2d6c90dbf31bc7a
StrikerOne65 <golamrabbani20014@gmail.com>
2022-04-09 02:05:59 +0530

    added count first beta

--- components/modal/data.js
+++ components/modal/data.js
@@ -1,15 +1,52 @@
 export const months = [
-  'Select Month',
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
+  'January',
+  'February',
+  'March',
+  'April',
   'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sep',
-  'Oct',
-  'Nov',
-  'Dec',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+
+export const example = [
+  {
+    name: 'Anish',
+    date: '16',
+    month: '4',
+  },
+  {
+    name: 'Rabbani',
+    date: '31',
+    month: '2',
+  },
+  {
+    name: 'ganesh',
+    date: '18',
+    month: '7',
+  },
+  {
+    name: 'Kaushik',
+    date: '31',
+    month: '0',
+  },
+  {
+    name: 'Anish Aditi',
+    date: '25',
+    month: '11',
+  },
+  {
+    name: 'Anish Padukone',
+    date: '11',
+    month: '8',
+  },
+  {
+    name: 'Aditi Padukone',
+    date: '9',
+    month: '3',
+  },
 ]
