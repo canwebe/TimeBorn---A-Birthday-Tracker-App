455358357b1ca46fb2548cfe031c17953fc218ad
StrikerOne65 <golamrabbani20014@gmail.com>
2022-04-12 23:17:37 +0530

    Added manifest json

--- pages/_document.js
+++ pages/_document.js
@@ -5,12 +5,20 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <meta name='theme-color' content='#6bcb77' />
+          <meta
+            name='description'
+            content='It is a birthday tracker app with added features like birthday wishes,notify events,searching friends and many more.'
+          />
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
           <link
-            href='https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap'
             rel='stylesheet'
           />
+
+          <link rel='apple-touch-icon' href='/logo192.png' />
+          <link rel='manifest' href='/manifest.json' />
         </Head>
         <body>
           <Main />
